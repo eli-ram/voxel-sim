3a6b5cbe48df6e837e0523ac1ed081386342d9ca
eliasr <eliasr@uio.no>
2022-03-04 02:37:21 +0100

    desktop transfer

--- source/loader/test.py
+++ source/loader/test.py
@@ -1,35 +1,52 @@
 # pyright: reportUnknownVariableType=false
 from typing import Any, Dict, Union
 from ..utils.directory import script_dir, cwd
+from dataclasses import dataclass
 import yaml
 
 Params = Union[Dict[str, Any], None]
 
+
+@dataclass
+class Object:
+    type: str
+    operation: str
+    properties: dict
+    transform: dict
+
+
 def title_print(title: str, data: Params):
     print(f"{title}:")
-    for k, v in (data or {}).items():    
+    for k, v in (data or {}).items():
         print(f"  {k}: {v}")
     print()
+
 
 def load_config(data: Params):
     title_print("Config", data)
 
+
 def load_colors(data: Params):
     title_print("Colors", data)
 
+
 def load_objects(data: Params):
-    title_print("Objects", data)
+    data = data or {}
+    objects = {key: Object(**data) for key, data in data.items()}
+    title_print("Objects", objects)
+
 
 def load_parameters(data: Params):
     title_print("Parameters", data)
 
+
 def load_configuration(data: Params):
-    if data is None:
-        return
+    data = data or {}
     load_config(data.get("config"))
     load_colors(data.get("colors"))
     load_objects(data.get("objects"))
     load_parameters(data.get("parameters"))
+
 
 @cwd(script_dir(__file__), '..', '..', 'configurations')
 def test_load():
