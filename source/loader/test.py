19ef2c2285892d0bdbe2211ae3e4ac6ea87826e9
eliasr <eliasr@student.matnat.uio.no>
2022-03-10 20:00:33 +0100

    it's time to integrate

--- source/loader/test.py
+++ source/loader/test.py
@@ -10,18 +10,25 @@ def load(parser: p.Parsable, filename: str) -> bool:
     with open(filename) as f:
         data = yaml.safe_load(f)
 
+    # Check if config corrected an error
+    had_error = bool(parser.error)
+
     print("\n[#] Parsing:", filename)
     parser.parse(data)
     fmt = Format(prefix=' |', list_unchanged=False, list_errors=True).init()
     print(f"[#] Result: \n V{parser.format(fmt)}\n A")
-    print(f"[#] Changed: {parser.changed}, Error: {bool(parser.error)}")
+    print(f"[#] Changed: {parser.changed}")
+    print(f"[#] Error: {bool(parser.error)}")
 
-    return parser.changed
+    # Check if config gained an error
+    has_error = bool(parser.error)
+
+    return not has_error and parser.changed or had_error
 
 
 @cwd(script_dir(__file__), '..', '..', 'configurations')
 def test_load():
-    files = ['test_1.yaml', 'test_2.yaml', 'test_2.yaml']
+    files = ['test_1.yaml', 'test_1.yaml', 'test_2.yaml', 'test_2.yaml']
     conf = Configuration()
     for file in files:
         load(conf, file)
