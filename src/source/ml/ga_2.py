b178a7fdda7ecb4531de6bfed665e58bdfac3ef9
elias <eliasr@uio.no>
2023-03-06 12:40:51 +0100

    ga_2 added using better storage mode

---
+++ src/source/ml/ga_2.py
@@ -0,0 +1,328 @@
+"""
+Genetic Algorithm
+
+1. population
+2. fitness
+3. selection
+4. reproduction
+5. mutation
+
+
+"""
+
+import os
+import glm
+import numpy as np
+from dataclasses import dataclass
+from datetime import datetime
+
+import source.ml.rng as r
+import source.math.fields as f
+import source.data.voxels as v
+import source.data.material as m
+import source.math.voxels2truss as v2t
+import source.math.truss2stress as fem
+import source.data.voxel_tree.node as n
+from source.loader.geometry import Context
+from source.utils.types import bool3, float3
+
+import source.ml.ga_storage as s
+
+Genome = s.SimpleGenome
+Storage = s.SimpleStorage()
+
+
+def new(a, b):
+    return s.Induvidual.new(Genome(glm.vec3(a), glm.vec3(b)))
+
+
+def random_genomes(rng, size: int):
+    A, B = np.split(r.make_unit_points(rng, size * 2), 2)
+    return [Genome(glm.vec3(a), glm.vec3(b)) for a, b in zip(A, B)]
+
+
+def random_induviduals(rng, size: int):
+    return [s.Induvidual.new(G) for G in random_genomes(rng, size)]
+
+
+# FIXME HOTFIX
+
+
+@dataclass
+class Config:
+    # rod transform
+    ctx: Context
+    # matrix: glm.mat4
+
+    # rod width
+    width: float
+
+    # rod endpoints
+    mat_a: glm.mat4
+    mat_b: glm.mat4
+
+    # rod material
+    material: m.Material
+
+    # rod operation
+    op: n.Operation
+
+    # input voxels
+    node: n.VoxelNode
+
+    # physics constraints
+    forces: dict[m.Material, float3]
+    statics: dict[m.Material, bool3]
+
+    # setup
+    seed: int | None
+    size: int
+
+    # output
+    folder: str
+
+    def seedPopulation(self, rng):
+        print("[config] creating a population of size", self.size)
+        return s.Generation(random_induviduals(rng, self.size), 0)
+
+    def iterPopulation(self, generation: s.Generation[Genome]):
+        # Cylinders between A & B
+        return (G for G in generation.population if not G.validated)
+
+    def presentInduvidual(self, genome: Genome):
+        # Create Cylinder field
+        field = f.Cylinder(
+            self.mat_a * genome.a,
+            self.mat_b * genome.b,
+        )
+
+        # field region
+        # B = self.node.data.box
+        ctx = self.ctx
+
+        # field transform
+        # matrix = glm.translate(-glm.vec3(*B.start)) * self.matrix
+
+        # Compute field
+        grid = field.compute(ctx.shape, ctx.matrix, self.width)
+
+        # Bundle as data w/ correct offset
+        data = n.Data.FromMaterialGrid(self.material, grid)
+
+        # Bundle as node
+        node = ctx.finalize(n.VoxelNode.Leaf(self.op, data))
+
+        # Join with computed node
+        return n.VoxelNode.process([self.node, node])
+
+    def createInduvidual(self, genome: Genome):
+        # creation & presentation uses same code
+        data = self.presentInduvidual(genome)
+
+        # build voxels
+        voxels = v.Voxels(data.material.shape)
+
+        # Set internals
+        voxels.grid = data.material
+        voxels.strength = data.strength
+        voxels.forces = self.forces
+        voxels.statics = self.statics
+
+        # done
+        return voxels
+
+    def evaluateInduvidual(self, individual: v.Voxels):
+        # TODO: multiprocess this function
+        # it's the easiest way to speedup the search
+
+        # build truss
+        truss = v2t.voxels2truss(individual)
+
+        try:
+            # sinmulate [todo: multiprocess this]
+            # {deformation, edge-compression}
+            D, E = fem.fem_simulate(truss)
+        except Exception as e:
+            print(e)
+            return 1E10
+
+        # unable to solve
+        if E is None:
+            return 1E10
+
+        # fitness modifiers
+        mods = 0.0
+
+        # detect nan values
+        N = np.isnan(E)
+
+        # fix nan values if present
+        # and punish bad solutions
+        if N.any():
+            E[N] = 0.0
+            mods = 1E2
+
+        # get min-max of compression
+        max = E.max()
+        min = E.min()
+
+        # get mean of stress
+        mean = abs(E).mean()
+
+        # print(" max:", max)
+        # print(" min:", min)
+        # print(" mean:", mean)
+
+        # fitness is based on
+        # minimizing distortion
+        # lower is better
+        fitness = abs(max) + abs(min) + mean + mods
+
+        # done
+        return fitness
+
+    def bestInduvidual(self, generation: s.Generation[Genome]):
+        # sort low to high
+        generation.sort()
+
+        # Lowest is best
+        return generation.population[0]
+
+    def selectPopulation(self, rng, generation: s.Generation[Genome]):
+        # sort low to high
+        generation.sort()
+
+        # fourths + rest
+        size = generation.size()
+        part = (size // 5)
+        rest = (size % 5) + part
+        # print(f"{size=} {part=} {rest=}")
+
+        # top indices
+        best = generation.population[:part]
+
+        # rng indices
+        # R = I[rng.integers(part, self.size, part)]
+        R = rng.integers(part, self.size, part)
+        rand = [generation.population[i] for i in R]
+        # print('R', len(R))
+
+        # rest
+        rest = random_induviduals(rng, rest)
+
+        # crossover
+        def crossover(A: list[s.Induvidual[Genome]], B: list[s.Induvidual[Genome]]):
+            return [new(a.genome.a, b.genome.b) for a, b in zip(A, B, strict=True)]
+
+        # build population w/ crossover
+        return s.Generation([
+            *best,
+            *rand,
+            *crossover(best, rand),
+            *crossover(rand, best),
+            *rest,
+        ], generation.index + 1)
+
+    def mutatePopulation(self, rng, generation: s.Generation[Genome]):
+        # mutation amount (* unit-rng)
+        amount = 10 / (1 + generation.index)
+
+        # size
+        size = generation.size()
+
+        # population
+        population = generation.population
+
+        # number of mutations
+        count = size // 4
+
+        # list of moves
+        moves = random_genomes(rng, count)
+
+        # cutoff position (ignore new individuals)
+        cutoff = (size // 5) + (size % 5)
+
+        # mutate gene
+        def mutate(g: glm.vec3, m: glm.vec3):
+            v = g + (m * amount)
+            l = glm.length(v)
+            return v / l if l > 1.0 else v
+
+        # indices of mutations
+        for i in rng.integers(0, cutoff, count):
+            I = population[i]
+            M = moves[i]
+            G = I.genome
+            # Mutate
+            G.a = mutate(G.a, M.a)
+            G.b = mutate(G.b, M.b)
+            # Invalidate
+            I.validated = False
+
+        # result
+        return generation
+
+    def getFolder(self):
+        now = datetime.now()
+        return f'{self.folder}{now:[%Y-%m-%d][%H-%M]}'
+
+
+class GA:
+
+    def __init__(self, config: Config):
+        self.running = False
+        self.reset(config)
+
+    def reset(self, config: Config):
+        self.rng = np.random.default_rng(config.seed)
+        self.config = config
+        self.generation = config.seedPopulation(self.rng)
+        self.best = None
+        self.db = s.Database(Storage, config.getFolder())
+
+    def current(self):
+        if best := self.best:
+            return self.config.presentInduvidual(best.genome)
+
+    def step(self):
+        R, self.running = self.running, True
+
+        # don't run twice ...
+        if R:
+            return
+
+        C = self.config
+
+        print(f"\n[generation-{self.generation.index}] running:")
+
+        # Iterate genomes
+        induviduals = C.iterPopulation(self.generation)
+        for i, I in enumerate(induviduals):
+            # print(f"[genome-{i}] evaluating:")
+            # Create induvidual
+            individual = C.createInduvidual(I.genome)
+            # Evaluate induvidual (fitness-function)
+            evaluation = C.evaluateInduvidual(individual)
+            # Store result
+            print(f"[genome-{i}] result: {evaluation:6.3f}")
+            I.fitness = evaluation
+
+        # Save progression data
+        self.db.save(self.generation)
+
+        # Update best result
+        best = C.bestInduvidual(self.generation)
+        if self.best is None or self.best.fitness > best.fitness:
+            self.best = best
+
+        # select population based on fitness
+        self.generation = C.selectPopulation(self.rng, self.generation)
+
+        # mutate population based on generation
+        self.generation = C.mutatePopulation(self.rng, self.generation)
+
+        # stop running
+        self.running = False
+
+        # present best induvidual
+        return C.presentInduvidual(self.best.genome)
--- src/source/ml/ga_storage.py
+++ src/source/ml/ga_storage.py
@@ -10,12 +10,14 @@ T = TypeVar('T')
 
 
 class Storage(Generic[T], Protocol):
+    """ Data storage serialization protocol """
     def deserialize(self, data: A) -> T: ...
     def serialize(self, value: T) -> A: ...
 
 
 @dataclass
 class Induvidual(Generic[T]):
+    """ A individual with a genome and a fitness """
     genome: T
     fitness: float
     validated: bool
@@ -30,6 +32,8 @@ class InduvidualStorage(Generic[T], Storage[Induvidual[T]]):
         self.genome = genome
 
     def serialize(self, value: Induvidual[T]) -> A:
+        assert value.validated, \
+            """ Trying to serialize a unvalidated induvidual """
         G = self.genome.serialize(value.genome)
         F = value.fitness
         return [F, *G]
@@ -45,10 +49,16 @@ class InduvidualStorage(Generic[T], Storage[Induvidual[T]]):
 
 @dataclass
 class Generation(Generic[T]):
+    """ A generation of induviduals """
     population: list[Induvidual[T]]
+    index: int
+
+    def size(self):
+        return len(self.population)
 
     def sort(self):
         self.population.sort(key=lambda i: i.fitness)
+
 
 
 class GenerationStorage(Generic[T], Storage[Generation[T]]):
@@ -58,15 +68,19 @@ class GenerationStorage(Generic[T], Storage[Generation[T]]):
     def serialize(self, value: Generation[T]) -> list[list[float]]:
         return [self.individual.serialize(I) for I in value.population]
 
-    def deserialize(self, data: list[list[float]]) -> Generation[T]:
-        return Generation([self.individual.deserialize(I) for I in data])
+    def deserialize(self, data: list[list[float]], index: int) -> Generation[T]:
+        return Generation([self.individual.deserialize(I) for I in data], index)
 
 
 class Database(Generic[T]):
+    """ A folder based storage for genetic algorithm generations """
     FILENAME = "generation.{}.values"
     REGEX = re.compile(r"^generation\.(\d+)\.values$")
 
     def __init__(self, storage: Storage[T], folder: str):
+        # Add subfolder
+        folder = os.path.join(folder, "ga")
+        
         # Setup internals
         self.__generations = 0
         self.__storage = GenerationStorage(storage)
@@ -86,13 +100,16 @@ class Database(Generic[T]):
         # [end] __init__
 
     def __file(self, generation: int):
+        """ Format the generation as a filepath """
         filename = self.FILENAME.format(generation)
         return os.path.join(self.__folder, filename)
 
     def generations(self):
+        """ Return the number of generations """
         return self.__generations
 
     def load(self, generation: int):
+        """ Load a specific generation from file """
         # Get file for generation
         file = self.__file(generation)
         assert os.path.isfile(file), \
@@ -109,30 +126,38 @@ class Database(Generic[T]):
         data = cast(list[list[float]], data)
 
         # Deserialize
-        return self.__storage.deserialize(data)
+        return self.__storage.deserialize(data, generation)
 
     def loadAll(self):
+        """ Load all generations from file """
         return [self.load(i) for i in range(self.__generations)]
 
     def loadLast(self):
+        """ Load last generation from file """
         if self.__generations == 0:
-            raise IndexError("No generations are saved")
+            raise IndexError("There is no generations")
         return self.load(self.__generations - 1)
 
     def save(self, generation: Generation[T]):
+        """ Save a generation to file as the new Last generation """
+        assert generation.index == self.__generations, \
+            f""" Generation has unexpected index (expected: {self.__generations} found: {generation.index})"""
         data = self.__storage.serialize(generation)
         file = self.__file(self.__generations)
         with open(file, "x") as f:
             f.writelines(" ".join(f"{v:.5f}" for v in R) + '\n' for R in data)
         self.__generations += 1
 
-    def hasGenerations(self):
-        return self.__generations > 0
+    def empty(self):
+        """ Check if no generations are saved """
+        return self.__generations == 0        
 
     def fitness(self, generations: 'list[Generation[T]]'):
+        """ Get the fitness array for a list of generations """
         return np.array([[I.fitness for I in G.population] for G in generations])
 
     def parameters(self, generations: 'list[Generation[T]]'):
+        """ Get the parameter array for a list of generations """
         S = self.__storage.individual.genome
         return np.array([[S.serialize(I.genome) for I in G.population] for G in generations])
 
@@ -179,14 +204,16 @@ if __name__ == '__main__':
             validated=True,
         )
 
-    G = Generation([new() for _ in range(8)])
+    G = Generation([new() for _ in range(8)], db.generations())
     db.save(G)
     print(db.generations())
 
     generations = db.loadAll()
     for i, g in enumerate(generations):
+        A = g.population[0].fitness
         g.sort()
-        print(i, g.population[0])
+        B = g.population[0].fitness
+        print(i, A, '->', B)
 
     F = db.fitness(generations)
 
